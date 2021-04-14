9f9c9a47eb59a95fafea69397a5cf7375358a4ae
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-14 08:35:47 -0400

    register

--- endpoints/register.js
+++ endpoints/register.js
@@ -8,7 +8,7 @@ module.exports.verify = function (req, res) {
 }
 
 module.exports.execute = function (req, res) {
-    for(const prop in req.body) {
+    for (const prop in req.body) {
         req.body[prop] = String(req.body[prop]);
     }
     if (req.body.password && req.body.username && req.body.firstname && req.body.lastname && req.body.email) {
@@ -17,23 +17,33 @@ module.exports.execute = function (req, res) {
         let firstname = req.body.firstname.trim();
         let lastname = req.body.lastname.trim();
         let username = req.body.username.trim();
-        if(!validateEmail(email)){
+        if (!validateEmail(email)) {
             res.status(400).send('Invalid email').end();
         }
-        else if(!validatePassword(password)) {
+        else if (!validatePassword(password)) {
             res.status(400).send('Password must be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character').end();
         }
-        else if(firstname == "" || lastname == "" || username == "") {
+        else if (firstname == "" || lastname == "" || username == "") {
             res.status(400).send('Empty fields').end();
         }
         else {
             sql.rawGet(`SELECT * FROM users WHERE email = "${email}" OR username = "${username}"`).then(row => {
-                if(row) {
+                if (row) {
                     res.status(400).send('Username or email is in use').end();
                 }
                 else {
                     let id = nanoid.nanoid();
-                    //insert into table
+                    bcrypt.hash(password, 10, function (err, hash) {
+                        if (err) {
+                            res.status(500).end();
+                        }
+                        else {
+                            sql.rawRun(`INSERT INTO users(username, email, password, id, firstname, lastname) VALUES("${username}", "${email}", "${hash}", "${id}", "${firstname}", "${lastname}")`).then(() => {
+                                //confirmation email goes here, when implemented
+                                res.status(200).send(id).end();
+                            }).catch(err => res.status(500).end());
+                        }
+                    })
                 }
             }).catch(err => res.status(500).end());
         }
@@ -49,6 +59,6 @@ function validateEmail(email) { //https://stackoverflow.com/a/46181
 }
 
 function validatePassword(password) { //https://stackoverflow.com/a/21456918
-    const re =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return re.test(String(password));
 }
