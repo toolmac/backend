07df041faf6d9d0fe14fe9701c4068b7bbe5ea98
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-14 15:44:06 -0400

    logout endpoint

--- endpoints/userlogout.js
+++ endpoints/userlogout.js
@@ -10,8 +10,20 @@ module.exports.verify = function (req, res) {
 }
 
 module.exports.execute = function (req, res) {
-    const { token } = req.body;
-    sql.rawRun(`DELETE FROM refresh WHERE token = "${token}"`).then(() => {
-        res.status(200).send('Logout success').end();
-    }).catch(err => res.status(500).end());
+    const refeshAuth = req.body.token;
+    if (authHeader) {
+        jwt.verify(refeshAuth, config.REFRESH_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            else {
+                sql.rawRun(`DELETE FROM refresh WHERE token = "${refeshAuth}"`).then(() => {
+                    res.status(200).send('Logout success').end();
+                }).catch(err => res.status(500).end());
+            }
+        });
+    }
+    else {
+        res.status(401).end();
+    }
 }
