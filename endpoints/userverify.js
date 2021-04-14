7efa5255e53ad5e3d02431528df608a9c638a706
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-14 18:36:17 -0400

    verify endpoint

---
+++ endpoints/userverify.js
@@ -0,0 +1,34 @@
+const sql = require('../lib/db');
+const helper = require('../lib/helper');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config.json');
+
+module.exports.name = "user/verify";
+module.exports.verify = function (req, res) {
+    return true;
+}
+
+module.exports.execute = function (req, res) {
+    /*
+    format: 
+    {
+        "code": "blah"
+    }
+    */
+    if (req.body.code) {
+        sql.rawGet(`SELECT * FROM verify WHERE code = "${req.body.code}"`).then(row => {
+            if (row) {
+                sql.rawRun(`UPDATE users SET verified = 1 WHERE id = "${row.id}"`).then(() => {
+                    res.status(200).json("You are verified!");
+                }).catch(err => res.status(500).json('Error'));
+            }
+            else {
+                res.status(401).json("Invalid verification code");
+            }
+        }).catch(err => res.status(500).json('Error'));
+    }
+    else {
+        res.status(401).json("Invalid verification code");
+    }
+}
