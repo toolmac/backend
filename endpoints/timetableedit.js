f7fc232337cbf32772e6c7b07926df8ae0b4f532
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-15 08:07:57 -0400

    check for timetable existence before update

--- endpoints/timetableedit.js
+++ endpoints/timetableedit.js
@@ -21,8 +21,17 @@ module.exports.execute = function (req, res) {
                 else {
                     if (req.body.timetable) {
                         if (/^[\],:{}\s]*$/.test(req.body.timetable.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-                            sql.runWithParams(`INSERT INTO timetables (id, json) VALUES(?, ?)`, [user.id, req.body.timetable]).then(() => {
-                                res.status(200).send(row.json);
+                            sql.rawGet(`SELECT * FROM timetables WHERE id = "${user.id}`).then(row => {
+                                if (row) {
+                                    sql.runWithParams(`UPDATE timetables SET json = ? WHERE id = ?`, [req.body.timetable, user.id]).then(() => {
+                                        res.status(200).json("Updated successfully");
+                                    }).catch(err => res.status(500).json('Error'));
+                                }
+                                else {
+                                    sql.runWithParams(`INSERT INTO timetables (id, json) VALUES(?, ?)`, [user.id, req.body.timetable]).then(() => {
+                                        res.status(200).json("Inserted successfully");
+                                    }).catch(err => res.status(500).json('Error'));
+                                }
                             }).catch(err => res.status(500).json('Error'));
                         } else {
                             res.status(400).json("Bad JSON");
