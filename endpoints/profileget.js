76f77e3a5bb4b54c0940518a82c33f70a44498a2
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-14 21:19:35 -0400

    profileget

---
+++ endpoints/profileget.js
@@ -0,0 +1,32 @@
+const sql = require('../lib/db');
+const helper = require('../lib/helper');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config.json');
+
+module.exports.name = "profile/get";
+module.exports.verify = function (req, res) {
+    return true;
+}
+
+module.exports.execute = function (req, res) {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        if (token) {
+            jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
+                if (err) {
+                    return res.sendStatus(403);
+                }
+                else {
+                    res.status(200).json(user);
+                }
+            });
+        }
+        else {
+            res.status(401).json('Missing auth header');
+        }
+    } else {
+        res.status(401).json('Missing auth header');
+    }
+}
