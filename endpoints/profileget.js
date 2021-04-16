f33fd58cad0c0bb02c5717b540077b675f6676bb
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-16 18:34:26 -0400

    responses for profileget endpoint

--- endpoints/profileget.js
+++ endpoints/profileget.js
@@ -16,7 +16,7 @@ module.exports.execute = function (req, res) {
         if (token) {
             jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
                 if (err) {
-                    return res.status(403).json("Unauthorized");
+                    return res.status(403).json({ status: 403, error: "Invalid or expired JWT token" });
                 }
                 else {
                     res.status(200).json(user);
@@ -24,9 +24,9 @@ module.exports.execute = function (req, res) {
             });
         }
         else {
-            res.status(401).json('Missing auth header');
+            res.status(401).json({ status: 401, error: "Missing authorization header contents" });
         }
     } else {
-        res.status(401).json('Missing auth header');
+        res.status(401).json({ status: 401, error: "Missing authorization header" });
     }
 }
